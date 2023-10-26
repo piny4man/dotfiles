db17571e03cb4217fdd5edc6669c5294f1f692e7
piny4man <development@pinya.dev>
2023-10-26 21:07:12 +0200

    feat: initial nvim customized setup

---
+++ qtile/.config/nvim/lua/editor.lua
@@ -0,0 +1,26 @@
+vim.opt.swapfile = false
+
+-- Show relative line numbers
+vim.opt.number = true
+vim.opt.rnu = true
+
+-- Show ruler
+vim.opt.ruler = true
+vim.opt.cc = "120"
+
+-- Search adjustments
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.smartcase = true
+vim.opt.showmatch = true
+
+-- Whitespace stuff
+vim.opt.wrap = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+-- Identing
+vim.opt.smartindent = true
+
