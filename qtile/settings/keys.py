6fa03668299f9445dc7e94eab7728b91647762d8
Alberto Mendez <development@pinya.dev>
2024-02-25 09:20:59 +0100

    Update keys.py

--- qtile/settings/keys.py
+++ qtile/settings/keys.py
@@ -42,7 +42,8 @@ keys = [
         lazy.layout.toggle_split(),
         desc='Toggle between split and unsplit sides of stack',
     ),
-    Key([mod], 'Return', lazy.spawn('wezterm'), desc='Launch terminal'),
+    Key([mod], 'Return', lazy.spawn('alacritty'), desc='Launch terminal'),
+    Key([mod, 'shift], 'Return', lazy.spawn('wezterm'), desc='Launch alternative terminal'),
     # Toggle between different layouts as defined below
     Key([mod], 'Tab', lazy.next_layout(), desc='Toggle between layouts'),
     Key([mod], 'w', lazy.window.kill(), desc='Kill focused window'),
