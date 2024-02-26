5f7f91dd1d32d942bcbf7e625540765355377054
piny4man <development@pinya.dev>
2024-02-26 20:47:00 +0100

    chore: update fish configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,12 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    set -gx PATH $HOME/bin /usr/local/bin $HOME/.local/bin /home/piny4man/.local/share/solana/install/active_release/bin $PATH
+    set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
+    set -gx BUN_INSTALL "$HOME/.bun"
+    set -gx PATH "$BUN_INSTALL/bin" $PATH
+
+    set -gx PYENV_ROOT "$HOME/.pyenv"
+    status --is-interactive; and command -v pyenv >/dev/null; or set -gx PATH "$PYENV_ROOT/bin" $PATH
+    pyenv init - | source
 end
 
 zoxide init fish | source
