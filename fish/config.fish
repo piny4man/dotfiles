bb7e1f1bbd24bec8ee2876fa16bc94be1d8e80b7
piny4man <development@pinya.dev>
2024-02-26 20:53:05 +0100

    chore: disable fish greeting

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
+set -g fish_greeting
+
 if status is-interactive
     set -gx PATH $HOME/bin /usr/local/bin $HOME/.local/bin /home/piny4man/.local/share/solana/install/active_release/bin $PATH
     set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
