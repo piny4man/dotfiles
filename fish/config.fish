5e57e0ce13a888b68588577f29a8430074ed3079
piny4man <development@pinya.dev>
2024-02-27 22:19:50 +0100

    chore: fix fish multiline prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,25 @@
 set -g fish_greeting
 
+function wrap_long_lines
+    set max_width (math (command printf "%d" (math (command tput cols))))  # Calculate available width
+    
+    set wrapped_prompt ''
+    for word in $STARSHIP_PS1
+        if test (string length -w $word) -gt $max_width
+            set _wrapped_part (string split -n $max_width $word)[1]
+            set wrapped_prompt $wrapped_prompt"$_wrapped_part\n"
+        else
+            set wrapped_prompt $wrapped_prompt$word' '
+        end
+    end
+    
+    echo -n $wrapped_prompt
+end
+
+function fish_prompt
+    wrap_long_lines
+end
+
 if status is-interactive
     set -gx PATH $HOME/bin /usr/local/bin $HOME/.local/bin /home/piny4man/.local/share/solana/install/active_release/bin $PATH
     set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
